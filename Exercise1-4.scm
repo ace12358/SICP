fbed9dd7d235073d1952ebec0b1599672372c0f6
北川善彬 <ace1235813@gmail.com>
2014-10-09 19:00:35 +0900

    Exercise1-4

--- Exercise1-4.scm
+++ Exercise1-4.scm
@@ -4,7 +4,6 @@
 ;;実行の仕方
 ;;gosh Exercise1-4.scm
 
-
 ;bserve that our model of evaluation allows for combinations whose operators are
 ;compound expressions. Use this observation to describe the behavior of the following procedure: 
 ;(define (a-plus-abs-b a b)
